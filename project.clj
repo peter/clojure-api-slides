c20ca03a86c26e52334d5fc0297a1a123ae431cd
Peter Marklund <peter@marklunds.com>
2016-06-10 10:25:47 +0200

    New slides on records, protocols, and schema

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clojure-api-slides "1.0.0"
+  :description "Slides on Building Clojure APIs"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [prismatic/schema "1.1.1"]
+                 [com.stuartsierra/component "0.3.1"]]
+  :min-lein-version "2.0.0")
